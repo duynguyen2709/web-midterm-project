aff41b3ea4142cf5b365741ae908c2d9608ee4b4
Taansduy <duyvt@ssg.vn>
2019-04-19 21:04:09 +0700

    create layout homepage

--- main-page/app.js
+++ main-page/app.js
@@ -3,24 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts=require('express-ejs-layouts');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use('/public', express.static('public'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.get('/', function(req, res) {
+  res.render('HomePage');
+});
+app.get('/user', function(req, res) {
+  res.render('/HomePage');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
