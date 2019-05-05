1aa09073eacc554eb1ab496a6801faf6bda7073a
duynguyen2709 <naduy.hcmus@gmail.com>
2019-05-05 11:46:27 +0700

    [admin-page] divide layout to subfolder

--- admin-page/controllers/orderController.js
+++ admin-page/controllers/orderController.js
@@ -1,5 +1,5 @@
 exports.index = function(req, res) {
-    res.render('layouts/order', { title: 'Admin Page' });
+    res.render('order/order', { title: 'Admin Page' });
 };
 
 // Display list of all orders.
