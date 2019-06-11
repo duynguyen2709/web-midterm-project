b856c3cf986e346da41532fc9ae40e27ff06386b
duy1998 <dinhcongduy1998@yahoo.com.vn>
2019-06-11 20:01:55 +0700

    filter Manufacturer

--- main-page/controllers/products.js
+++ main-page/controllers/products.js
@@ -3,15 +3,26 @@ var product = require('../model/product')
 var user = require('./user')
 
 exports.product_list = async function (req, res) {
-
-    console.log("get into product_list");
     const categoryID = req.body.categoryID;
-
+    var listProductReal=new Array();
     const listProduct = await product.getProductOfCategory(categoryID);
-
-    res.json({
-        info: listProduct
-    });
+    if(req.body.filterName=="Tất cả"){
+        res.json({
+            info: listProduct
+        });
+    }
+    else{   
+        for (let i=0;i < listProduct.length ;i++){
+        if(listProduct[i].manufacturer===req.body.filterName)
+        listProductReal.push(listProduct[i]);
+        }
+        res.json({
+            info: listProductReal
+        });
+        console.log(listProductReal);
+    }
+    
+    
 }
 
 // Display list of all products of specific type.
@@ -26,36 +37,60 @@ exports.product_list_type = async function (req, res) {
         listCategory: listCategories,
         user: curUser,
         length: listProduct.length,
-        type: type
+        type: type,
+        listType: null,
+        manufaceturerName: null
     });
 };
 
 // Display list of all products of specific type (Filter).
 exports.product_list_type_filter = async function (req, res) {
-    
+    // let unique = [...new Set(listType)];
+    // let listTypeReal=Array();
+    // listTypeReal=Array.from(unique);
+    var body;
+    var listType=[];
     let listCategories = await category.getListCategory();
     var type = req.body.type;
     let curUser = req.session.user;
     let valueFilter=req.body.value;
-    var listProduct = await product.getProductOfCategory(type);
+    const listProduct = await product.getProductOfCategory(type);
     var listProduct1= new Array();
     var listProductReal=new Array();
     listProduct1=listProduct;
-    console.log(valueFilter);
-    
     for (let i=0;i < listProduct1.length ;i++){
+        listType.push(listProduct1[i].manufacturer);
         if(listProduct1[i].manufacturer===valueFilter)
         listProductReal.push(listProduct1[i]);
     }
-    console.log(listProductReal);
-    console.log(listProductReal.length);
-    res.render('product/product', {
+    var uniq = [...new Set(listType)];
+    // var tmp=listType.push("asd");
+    // console.log(JSON.stringify(listType));
+    
+    if(valueFilter=="Tất cả"){
+        res.render('product/product', {
+            info: listProduct,
+            listCategory: listCategories,
+            user: curUser,
+            length: listProduct.length,
+            type: type,
+            listType : null,
+            manufaceturerName: null
+        });
+    }
+    else{
+        res.render('product/product', {
         info: listProductReal,
         listCategory: listCategories,
         user: curUser,
         length: listProductReal.length,
-        type: type
+        type: type,
+        listType : uniq,
+        manufaceturerName:req.body.filterName
     });
+    }
+    
+    
 };
 
 // Display detail page for a specific product.
