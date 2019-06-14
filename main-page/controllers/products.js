6b3d01000bbabab9db4eacf3311ac738d42aae00
duyna5 <duyna5@vng.com.vn>
2019-06-14 09:01:54 +0700

    refactor code

--- main-page/controllers/products.js
+++ main-page/controllers/products.js
@@ -1,28 +1,28 @@
 var category = require('../model/category')
 var product = require('../model/product')
 var user = require('./user')
+var axios = require('axios');
 
 exports.product_list = async function (req, res) {
     const categoryID = req.body.categoryID;
-    var listProductReal=new Array();
+    var listProductReal = new Array();
     const listProduct = await product.getProductOfCategory(categoryID);
-    if(req.body.filterName=="Tất cả"){
+    if (req.body.filterName == "Tất cả") {
         res.json({
             info: listProduct
         });
-    }
-    else{   
-        for (let i=0;i < listProduct.length ;i++){
-        if(listProduct[i].manufacturer===req.body.filterName)
-        listProductReal.push(listProduct[i]);
+    } else {
+        for (let i = 0; i < listProduct.length; i++) {
+            if (listProduct[i].manufacturer === req.body.filterName)
+                listProductReal.push(listProduct[i]);
         }
         res.json({
             info: listProductReal
         });
         console.log(listProductReal);
     }
-    
-    
+
+
 }
 
 // Display list of all products of specific type.
@@ -49,48 +49,47 @@ exports.product_list_type_filter = async function (req, res) {
     // let listTypeReal=Array();
     // listTypeReal=Array.from(unique);
     var body;
-    var listType=[];
+    var listType = [];
     let listCategories = await category.getListCategory();
     var type = req.body.type;
     let curUser = req.session.user;
-    let valueFilter=req.body.value;
+    let valueFilter = req.body.value;
     const listProduct = await product.getProductOfCategory(type);
-    var listProduct1= new Array();
-    var listProductReal=new Array();
-    listProduct1=listProduct;
-    for (let i=0;i < listProduct1.length ;i++){
+    var listProduct1 = new Array();
+    var listProductReal = new Array();
+    listProduct1 = listProduct;
+    for (let i = 0; i < listProduct1.length; i++) {
         listType.push(listProduct1[i].manufacturer);
-        if(listProduct1[i].manufacturer===valueFilter)
-        listProductReal.push(listProduct1[i]);
+        if (listProduct1[i].manufacturer === valueFilter)
+            listProductReal.push(listProduct1[i]);
     }
     var uniq = [...new Set(listType)];
     // var tmp=listType.push("asd");
     // console.log(JSON.stringify(listType));
-    
-    if(valueFilter=="Tất cả"){
+
+    if (valueFilter == "Tất cả") {
         res.render('product/product', {
             info: listProduct,
             listCategory: listCategories,
             user: curUser,
             length: listProduct.length,
             type: type,
-            listType : null,
+            listType: null,
             manufaceturerName: null
         });
-    }
-    else{
+    } else {
         res.render('product/product', {
-        info: listProductReal,
-        listCategory: listCategories,
-        user: curUser,
-        length: listProductReal.length,
-        type: type,
-        listType : uniq,
-        manufaceturerName:req.body.filterName
-    });
+            info: listProductReal,
+            listCategory: listCategories,
+            user: curUser,
+            length: listProductReal.length,
+            type: type,
+            listType: uniq,
+            manufaceturerName: req.body.filterName
+        });
     }
-    
-    
+
+
 };
 
 // Display detail page for a specific product.
@@ -100,6 +99,16 @@ exports.product_detail = async function (req, res) {
     const productInfo = await product.getProductDetail(productId)
     let curUser = null
     curUser = user.currentUser()
+
+    axios({
+            method: 'PUT',
+            url: 'https://api-scttshop-v2.herokuapp.com/api/products/' + productId + '/view'
+        })
+        .then(response => {})
+        .catch(err => {
+            console.log(err);
+        });
+
     res.render('product/product_detail', {
         productInfo: productInfo,
         listCategory: listCategories,
--- main-page/model/product.js
+++ main-page/model/product.js
@@ -34,6 +34,7 @@ exports.getProductOfCategory = async (categoryID) => {
 
         PRODUCT_OF_CATEGORIES_CACHE.set(categoryID, data);
     }
+
     return PRODUCT_OF_CATEGORIES_CACHE.get(categoryID);
 }
 
